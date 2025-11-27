f9021991bf0ec5bcdd8e4fc652fdefce3b06cacc
udaykiran2427 <udaykiranpersonal77@gmail.com>
2025-11-27 23:23:43 +0530

    restructured the repo

--- app/schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-#input text is string
-class TextInput(BaseModel):
-    text: str
-#output is list of strings
-class PredictionOut(BaseModel):
-    predicted_genres: list[str]
